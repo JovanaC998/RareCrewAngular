47d02015dcfb959f6441b08c1bab6c430e6fae64
JovanaC998 <jovanaciric701@gmail.com>
2024-08-20 00:57:21 +0200

    Point b) of the task is complete. A pie chart of employees has been displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeesTableComponent } from './employees-table/employees-table.component';
-import { CommonModule } from '@angular/common';
+import { EmployeesChartComponent } from './employees-chart/employees-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeesTableComponent, CommonModule],
+  imports: [RouterOutlet, EmployeesTableComponent, EmployeesChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
